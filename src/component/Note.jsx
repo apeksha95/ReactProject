e716ded24d5d87776e67b2e57643b363285b285d
Apeksha Kale <53891161+apeksha95@users.noreply.github.com>
2021-02-04 23:05:37 +0530

    complete project

--- src/component/Note.jsx
+++ src/component/Note.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
 
-function Notes() {
+function Note(props) {
+  function handleClick() {
+    props.onDelete(props.id);
+  }
+
   return (
     <div className="note">
-      <h1>This is the note title</h1>
-      <p>This is the note content</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}>
+        <DeleteIcon />
+      </button>
     </div>
   );
 }
 
-export default Notes;
+export default Note;
